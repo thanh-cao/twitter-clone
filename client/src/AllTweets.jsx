10d6a17b789ec7c8f637afde168521ad6ba37c02
Thanh <55603278+thanh-cao@users.noreply.github.com>
2022-02-09 12:51:26 +0100

    create Tweet component

--- client/src/AllTweets.jsx
+++ client/src/AllTweets.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Tweet from './Tweet';
 
 export default class AllTweets extends Component {
     constructor(props) {
@@ -12,44 +13,46 @@ export default class AllTweets extends Component {
         const response = await fetch('http://localhost:3005/tweets');
         const tweets = await response.json();
         this.setState({ tweets });
-        console.log(tweets);
     }
 
     render() {
         const currentTimestamp = new Date().getTime() / 1000;
-        
+
         const calculateTime = (createdAt) => {
-            createdAt = Date.parse(createdAt)/1000;
+            createdAt = Date.parse(createdAt) / 1000;
             const time = currentTimestamp - createdAt;
 
-            switch(true) {
+            switch (true) {
                 case time < 60:
                     return `${time} seconds ago`;
                 case time < 3600:
-                    return `${Math.floor(time/60)} minutes ago`;
+                    return `${Math.floor(time / 60)} minutes ago`;
                 case (time < 86400):
-                    return `${Math.floor(time/3600)} hours ago`;
+                    return `${Math.floor(time / 3600)} hours ago`;
                 case time < 604800:
-                    return `${Math.floor(time/86400)} days ago`;
+                    return `${Math.floor(time / 86400)} days ago`;
                 case time < 2592000:
-                    return `${Math.floor(time/604800)} weeks ago`;
+                    return `${Math.floor(time / 604800)} weeks ago`;
                 case time < 31536000:
-                    return `${Math.floor(time/2592000)} months ago`;
+                    return `${Math.floor(time / 2592000)} months ago`;
                 default:
-                    return `${Math.floor(time/31536000)} years ago`;
+                    return `${Math.floor(time / 31536000)} years ago`;
             }
         };
 
         return (
             <div>
                 <h1>All Tweets</h1>
+
                 {this.state.tweets.map(tweet => (
-                    <div key={tweet.id}>
-                        <img src="https://via.placeholder.com/150" alt="profile" width="50"/>
-                        <h2>{tweet.User.name} @{tweet.User.username}</h2>
-                        <p>{tweet.message}</p>
-                        <p>{calculateTime(tweet.createdAt)}</p>
-                    </div>
+                    <Tweet
+                        key={tweet.id}
+                        name={tweet.User.name}
+                        username={tweet.User.username}
+                        avatar="https://via.placeholder.com/150"
+                        message={tweet.message}
+                        createdAt={calculateTime(tweet.createdAt)}
+                    />
                 ))}
             </div>
         );
